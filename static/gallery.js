2d9c32c6d4397c3b0870edb6cfafb64a23f45767
helveticalover <helveticalover@users.noreply.github.com>
2022-11-03 05:57:45 +0000

    deploy: 923f0e2e9abb6de35a9d0cde2e06fb88c6a31f9e

--- static/gallery.js
+++ static/gallery.js
@@ -1,4 +1,4 @@
-const TARGET_HEIGHT = 400;
+const PADDING = 10;
 
 function logElementEvent(eventName, element) {
     console.log(Date.now(), eventName, element.getAttribute("data-src"));
@@ -65,7 +65,7 @@ function sizeImages(container)
     console.log("=== RESIZING IMAGES ===");
 
     // TODO: move this to a config
-    let p = 10;
+    const TARGET_HEIGHT = 500;
 
     let rect = container.getBoundingClientRect();
     let R;
@@ -78,8 +78,6 @@ function sizeImages(container)
         R = Math.floor(rect.right - rect.left);
     }
 
-    console.log("Target row width: ", R);
-
     let sumr = [];
     sumr[0] = {
         sum: 0,
@@ -87,34 +85,54 @@ function sizeImages(container)
     };
     let dat = sumr[sumr.length - 1];
 
+    let deviation = Number.MAX_VALUE;
     let imgs = container.getElementsByTagName("img");
     for (let img of imgs)
     {
         let h = img.dataset.height;
         let w = img.dataset.width;
         let w_h = w / h;
-        let sum_1 = dat.sum + w_h;
 
-        if (dat.images.length > 0 && TARGET_HEIGHT * sum_1 > R)
+        // Try add image to existing row
+        dat.sum += w_h;
+        dat.images.push(img);
+        calculateScaledRows(sumr, R);
+
+        let samples = sumr.map((dat) => dat.scaledHeight);
+        let newDeviation = getDeviation(samples, TARGET_HEIGHT);
+
+        // If adding image to row increases deviation, add it to new row
+        if (newDeviation > deviation)
         {
+            dat.sum -= w_h;
+            dat.images.pop();
+
             console.log("Finished row: ", dat.images.length, dat.sum * TARGET_HEIGHT);
             dat = {};
             dat.sum = w_h;
             dat.images = [img];
             sumr.push(dat);
+
+            calculateScaledRows(sumr, R);
+            samples = sumr.map((dat) => dat.scaledHeight);
+            newDeviation = getDeviation(samples, TARGET_HEIGHT);
         }
-        else
-        {
-            dat.sum += w_h;
-            dat.images.push(img);
-        }
+
+        deviation = newDeviation;
+        console.log("Deviation", deviation);
     }
+    // console.log(sumr);
 
-    console.log("Finished row: ", dat.images.length, dat.sum * TARGET_HEIGHT);
+    scaleRowsToTarget(sumr);
 
-    for (let dat of sumr)
+    return sumr;
+}
+
+function calculateScaledRows(rows, R)
+{
+    for (let dat of rows)
     { 
-        let R_1 = R - 2 * dat.images.length * p;
+        let R_1 = R - 2 * dat.images.length * PADDING;
 
         for (let img of dat.images)
         {
@@ -125,12 +143,43 @@ function sizeImages(container)
             let div = h * dat.sum;
             let w_1 = Math.floor(num / div);
             let h_1 = Math.floor(w_1 * h / w);
-
-            console.log(w_1, h_1);
-
-            img.width = w_1;
-            img.height = h_1;
-            img.style.padding = p + "px";
+            dat.scaledHeight = h_1;
         }
     }
+}
+
+function scaleRowsToTarget(rows)
+{
+    for (let dat of rows)
+    { 
+        for (let img of dat.images)
+        {
+            img.width = dat.scaledHeight * img.dataset.width / img.dataset.height;
+            img.height = dat.scaledHeight;
+            img.style.padding = PADDING + "px";
+        }
+    }
+}
+
+function getMean(samples)
+{
+    let sum = 0;
+    let N = samples.length;
+    for (let x of samples)
+    {
+        sum += x;
+    }
+    return sum / N;
+}
+
+function getDeviation(samples, mean)
+{
+    let denom = samples.length;
+    let deviation = 0;
+    for (let x of samples)
+    {
+        deviation = (x - mean) * (x - mean);
+    }
+    deviation /= (denom - 1);
+    return Math.sqrt(deviation);
 }
