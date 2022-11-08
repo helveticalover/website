a9a8597b62cdf51b7c6fb816217a10ce5b82765e
Tiffany Li <25678112+helveticalover@users.noreply.github.com>
2022-11-07 20:21:39 -0800

    Get project thumbnails working

--- projects.11ty.js
+++ projects.11ty.js
@@ -2,11 +2,22 @@ exports.data = {
     layout: "main.njk",
 }
 
-exports.render = function(data) {
+exports.render = async function(data) {
+    let projects = await Promise.all(data.collections.project.map(async function (project) {
+        if (project.data.thumbnail)
+        {
+            let img = await exports.image("static/images/" + project.data.thumbnail, project.data.blurb);
+            return `<li><a href="${exports.url(project.url)}">${img}</a></li>`;
+        }
+        else
+        {
+            return `<li><a href="${exports.url(project.url)}">${project.data.title}</a></li>`;
+        }
+    })).then(function (projects) {
+        return projects.join("\n");
+    });
     return `<ul>
-        ${data.collections.project.map(function (project) {
-            return `<li><a href="${exports.url(project.url)}">${project.data.title}</li></a>`
-        }).join("\n")}
+        ${projects}
     </ul>`;
 }
 
