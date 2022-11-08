e8992824d2da3991fa0ccfcb5c5730c268edc7ea
helveticalover <helveticalover@users.noreply.github.com>
2022-11-08 06:15:13 +0000

    deploy: e4548e0d20e4d7f1f3f89bd0b90ce3cf90976ef7

---
+++ static/modal/modal.js
@@ -0,0 +1,29 @@
+var modals = document.getElementsByClassName("modal");
+
+for (let modal of modals)
+{
+    var background = modal.querySelector(".background");
+    var modalImg = modal.querySelector(".modal-content");
+    var captionText = modal.querySelector(".modal-caption");
+    
+    function showInModal(src, alt) {
+        modalImg.src = src;
+        captionText.innerHTML = alt;
+        modalImg.onload = function () {
+            modal.classList.remove("hidden");
+            modal.classList.add("displayed");
+        }
+    }
+    
+    var span = modal.querySelector(".close");
+    span.onclick = function() {
+        modal.classList.remove("displayed");
+        modal.classList.add("hidden");
+    }
+    
+    background.onclick = span.onclick;
+    
+    modalImg.onclick = function(e) {
+        e.stopPropagation();
+    }
+}
